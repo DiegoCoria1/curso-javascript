eb98c7f92eb2b53db0020d4837b185320dcf0f17
DiegoCoria1 <diegofelipe.coria@alumnos.ulagos.cl>
2024-09-05 01:32:11 +0200

    Se agrega documento JS de prueba para Global Scope

---
+++ globalscope.js
@@ -0,0 +1,15 @@
+var comic = "x-men"
+
+function comprar () {
+console.log("Compraste el comic ${comic}")
+//Compraste el comic x-men
+}
+function leer () {
+    console.log("Estas leyendo ${comic}")
+    //Estas leyendo x-men
+}
+
+comprar ()
+leer()
+
+//El scope global nos permite definir variables que se puedan llamar desde  todo el código
