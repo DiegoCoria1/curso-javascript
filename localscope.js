f46f5dcb68bd1942b09f0c241f3799fff27abd9c
DiegoCoria1 <diegofelipe.coria@alumnos.ulagos.cl>
2024-09-05 01:32:30 +0200

    Se agrega documento JS para probar Local Scope

---
+++ localscope.js
@@ -0,0 +1,11 @@
+function teamCap () {
+    var heroe = "Capitan America";
+    console.log(heroe);
+    //Capitan America
+}
+function teamIronMan () {
+    var heroe = "Iron Man";
+    console.log(heroe);
+    //Iron Man
+}
+// En cada función nos entrega un heroe diferente
