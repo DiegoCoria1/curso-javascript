70d309d16d408332e72a41473b3908efec86dcdb
DiegoCoria1 <diegofelipe.coria@alumnos.ulagos.cl>
2024-09-27 21:35:18 +0200

    Se sube ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+let numero = prompt("Ingrese un Número") //solicitamos un numero al usuario
+    //definimos un condicional para saber si el numero es par o impar
+if (numero % 2  == 0 ){ //si el numero tiene como resultado 0 al aplicarle módulo 2 el resultado será par
+    alert("el " + numero + " es par") 
+}
+else { //si el resultado no es 0, será impar
+    alert("el " + numero + " es impar")
+}
+
