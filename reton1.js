1e5e47c57a6acf2e459bdad83e3f3a899c53ecaf
DiegoCoria1 <diegofelipe.coria@alumnos.ulagos.cl>
2024-09-27 21:36:57 +0200

    Se sube reto 1

---
+++ reton1.js
@@ -0,0 +1,16 @@
+let frase = prompt('Ingrese una Cadena de Texto', '')
+let X = 0
+let letra = frase.charAt(X)
+let contador = 0
+let longitud = frase.length
+
+
+if (letra === a, e, i, o, u) {
+    contador = contador + 1;
+    X = X + 1;
+}
+else {( X = X + 1)
+}
+
+alert("Tienes " + contador + " Vocales");
+
