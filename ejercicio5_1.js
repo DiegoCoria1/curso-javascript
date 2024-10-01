c44293c7dc62cf48a6e157734e0f0cf2c668a057
DiegoCoria1 <diegofelipe.coria@alumnos.ulagos.cl>
2024-10-02 01:33:28 +0200

    Se sube ejercicio 5, no tener en cuenta.

---
+++ ejercicio5_1.js
@@ -0,0 +1,7 @@
+function ejercicioUno (){
+    let frase = prompt("Ingrese una Frase")
+    let contadorCaracteres = frase.length
+    alert("La frase es : " + frase + "; y la cantidad de caracteres es : " + contadorCaracteres)
+}
+ejercicioUno();
+
